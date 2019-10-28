1886bcb31337a946a13735373a2a836fad927b35
Talbot French <talbot.french@gmail.com>
2019-10-28 17:42:48 -0600

    Add Daily Report
    new Daily report

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Acme
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're 'Accounting' on you!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dailyReport.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What Course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("What page are you on?");
+            string coursePage = Console.ReadLine();
+            Console.WriteLine("Do you need help?");
+            String help = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.?");
+            String positive = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            String feedBack = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            String studyHours = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+           
+
+
+        }
+    }
+}
