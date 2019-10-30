a5d1b3e668ba56fff1fc5ac4e723b6e0ab7ae87c
Talbot French <talbot.french@gmail.com>
2019-10-30 17:09:47 -0600

    new while loops

---
+++ While.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _94Proj
+{
+    class Program
+    {
+        static void Main()
+        {
+            var result = "";
+            var i = 5;
+            do
+            {
+                i = i - 1;
+                result = result + i + " tries.";
+                bool Gender = false;
+                while (Gender == false)
+                {
+                    Console.Write("Enter your Gender: ");  // Asks for your name
+                    string myGender = Console.ReadLine();
+                    if (myGender == "Male")
+                    {
+                        Console.WriteLine("You are a Male");
+                        Gender = true;
+                    }
+
+                    else if (myGender == "Female")
+                    {
+                        Console.WriteLine("You are a Female");
+                        Gender = true;
+
+                    }
+
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Please enter a valid gender.");
+                        Gender = false;
+
+                    }
+                    Console.ReadLine();
+                }
+
+
+
+            } while (i > 0);
+            Console.WriteLine(result);
+        }
+    }
+}
