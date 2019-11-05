4d8fe43a3aebdca6d2b1cc5ea6bf3000e211bb80
Talbot French <talbot.french@gmail.com>
2019-11-05 15:25:41 -0700

    Create Void.cs

---
+++ Void.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Thing stuff = new Thing();
+            int a = 5;
+            int b = 7;
+            int Resultb = stuff.Stuff(a, b); 
+
+        }
+
+    }
+}
