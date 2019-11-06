1ba34727bada9b5d704cdf4a912b36e826cd0c50
Talbot French <talbot.french@gmail.com>
2019-11-06 14:30:56 -0700

    Practice with methods 115
    method is Method115

---
+++ 115.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method math = new Method();
+            Console.WriteLine("Enter a number.");
+            int numA = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter a second number if you desire.");
+            string numB = Console.ReadLine();
+
+            int a;
+            if (Int32.TryParse(numB, out a))
+            {
+
+                Console.WriteLine(math.AddNumbers(numA, a));
+            }
+            else
+            {
+                Console.WriteLine(math.AddNumbers(numA));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Method115.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    public class Method       
+    {
+        
+            
+        public int AddNumbers(int numA, int numB = 0)
+        {
+            return numA + numB;
+           
+        }
+
+    }
+}
