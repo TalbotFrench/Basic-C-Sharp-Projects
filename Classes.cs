2a7f8a24ce90ec100da8b542d64a55c45aa9ec36
Talbot French <talbot.french@gmail.com>
2019-11-05 17:18:46 -0700

    Create Classes.cs

---
+++ Classes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Math math = new Math();
+            int twelve = math.Add(12);
+            Console.WriteLine(twelve);
+
+            double div = math.Div(7.9);
+            Console.WriteLine(div);
+
+            string multi = Convert.ToInt32(math.Multi(5));
+            Console.WriteLine(div);
+
+
+        }
+    }
+}
