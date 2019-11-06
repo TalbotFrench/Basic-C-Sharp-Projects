24aa86faf74f65b777e53da5af0ef41294e97321
Talbot French <talbot.french@gmail.com>
2019-11-06 15:44:29 -0700

    117
    i have a problem calling the method but i think the code is correct for the most part

---
+++ 117.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DivideByTwo div = new DivideByTwo();
+
+            Console.WriteLine("Enter a number");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(div.Divide(A));
+
+
+        }
+    }
+}
---
+++ 117Method.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _117
+{
+    public class DivideByTwo
+    {
+        public void Divide(int numA)
+        {
+           int result =  numA / 2;
+        }
+
+        public void Divide(int numA, int numB)
+        {
+            int result = (numA + numB ) / 2;
+        }
+
+        void OutPut(out int number)
+        {
+            number = 55;
+        }
+
+        public static int numG;
+        static void Main()
+        {
+            numG = 10;
+            Console.WriteLine(numG);
+        }
+    }
+
+
+}
