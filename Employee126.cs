674ab132435f7b67fbc5468190a73d8dce68044b
Talbot French <talbot.french@gmail.com>
2019-11-07 13:44:27 -0700

    drill 126
    IQuittable126 and Employee126

---
+++ Employee126.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullName
+{
+    public class Employee : Person, IQuittable
+    {
+
+        Person p = new Person("Sample", "Student");
+        Console.WriteLine(p.SayName());
+        int id = 0001;
+
+        void Quit(string a)
+        {
+            string a = "You quit!";
+            Console.WriteLine(a);
+        }
+
+        void Quit(string a, int b)
+        {
+            string a = "You quit";
+            Console.WriteLine(a + b +" times");
+        }
+
+
+    }
+}
