787428aa394f39b0b89a05dfaf681623f734837a
Talbot French <talbot.french@gmail.com>
2019-11-08 12:09:43 -0700

    Drill 129
    129 main and Employee129

---
+++ 129Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Talbot = new Employee();
+            Employee employee2 = new Employee();
+
+            Talbot.Id = 0001;
+            Talbot.Name = "Talbot";
+
+            employee2.Id = 0002;
+            employee2.Name = "Benhem";
+
+            bool isEmployee = Talbot == employee2;
+
+            Console.WriteLine("Does " + Talbot.Name + " and " + employee2.Name + "s employee Ids match? " + isEmployee);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Employee129.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees129
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+            public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
