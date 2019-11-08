73e78445e6fa03bbd07df678f92a8bde606629ed
Talbot French <talbot.french@gmail.com>
2019-11-08 16:17:42 -0700

    Create DaysOfTheWeek.cs
    having problems finding a way to convert to enum to string and all the tutorial focus on integers or are super confusing

---
+++ DaysOfTheWeek.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Please enter a day of the week:");
+            string userInput = Console.ReadLine();
+
+
+
+            try
+            {
+                userInput == Enum.TryParse());
+
+
+            }
+            catch (InvalidCastException e){
+                Console.WriteLine("Please enter a valid day");
+            }
+
+            
+        }
+        public enum Days
+        {
+            Monday,
+            Tuesday,
+            Wensday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        
+    }
+}
