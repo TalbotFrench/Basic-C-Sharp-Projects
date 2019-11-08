ac406320717e3cad8c48a01f8fac22bb59e683c6
Talbot French <talbot.french@gmail.com>
2019-11-08 13:42:20 -0700

    131
    Employee131 and 131

---
+++ 131Main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> Names = new Employee<string>(); 
+            Employee<int> EmployeesId = new Employee<int>();
+
+            Names.things.Add("Yeonce");
+            Names.things.Add("Wayne");
+            Names.things.Add("Cole");
+
+
+            EmployeesId.things.Add(0001);
+            EmployeesId.things.Add(0002);
+            EmployeesId.things.Add(0003);
+
+            foreach (string item in Names.things)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (int item in EmployeesId.things)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Employee Talbot = new Employee();
+            //Employee employee2 = new Employee();
+
+            //Talbot.Id = 0001;
+            //Talbot.Name = "Talbot";
+
+            //employee2.Id = 0002;
+            //employee2.Name = "Benhem";
+
+            //bool isEmployee = Talbot == employee2;
+
+            //Console.WriteLine("Does " + Talbot.Name + " and " + employee2.Name + "s employee Ids match? " + isEmployee);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ Employee131.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees129
+{
+    public class Employee<T>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<T> things { get; set; }
+
+        //public static bool operator ==(Employee employee1, Employee employee2)
+        //{
+        //    if (employee1.Id == employee2.Id)
+        //    {
+        //        return true;
+
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        
+
+        //    public static bool operator !=(Employee employee1, Employee employee2)
+        //{
+        //    if (employee1.Id != employee2.Id)
+        //    {
+        //        return true;
+
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+    }
+}
