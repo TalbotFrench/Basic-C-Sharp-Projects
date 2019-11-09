97c585215029930ee12239a3c10122a627a11c2e
Talbot French <talbot.french@gmail.com>
2019-11-08 17:21:34 -0700

    Create 137cs.cs

---
+++ 137cs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number(5);
+
+            Console.WriteLine(num);
+
+      
+
+        }
+        public struct Number {
+            public decimal Amount(decimal a);
+        }
+
+}
+}
