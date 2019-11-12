2e2111bb653ccb33d9bcbb449eaf8239f57dc68a
Talbot French <talbot.french@gmail.com>
2019-11-12 11:30:14 -0700

    Create LogNum.cs

---
+++ LogNum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\talbo\logs\log.txt", true))
+            {
+                file.WriteLine(num);
+            }
+            Console.WriteLine(num);
+        }
+    }
+}
