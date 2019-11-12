aa31a5f7e9ea1542681ed5ec2077cdb8fa881542
Talbot French <talbot.french@gmail.com>
2019-11-12 12:11:23 -0700

    Create HoursDateTime.cs

---
+++ HoursDateTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Please input a number");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            DateTime dateTime = new DateTime(2019, 11, 12, hours, 6, 30);
+
+
+
+        }
+    }
+}
