a5135ebdb4489e4cc50f3b20610772a0a0d95aa0
Talbot French <talbot.french@gmail.com>
2019-11-12 17:29:14 -0700

    162
    drill 162

---
+++ 162.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant162
+{
+    
+    class Program
+
+        
+
+    {
+        
+        public const string ConnectionString = "YourConnectionString";
+        public int runner;
+
+        static void Main(string[] args)
+        {
+            var runner = 67;
+        }
+       
+        
+    }
+}
---
+++ Cam162.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant162
+{
+    public class Cam
+    {
+        string _name = "";
+        string _age = "";
+   
+    public Cam(string name)
+    {
+        _name = name;
+    }
+    public Cam(string name, string age)
+    {
+        _name = name;
+        _age = age;
+    }
+}
+}
