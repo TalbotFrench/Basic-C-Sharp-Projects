98249a0e32ace317a381c0301298d8c909e54c6d
Talbot French <talbot.french@gmail.com>
2019-12-17 16:25:34 -0700

    113try2

---
+++ 113Method2/113Method2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _113Method2
+{
+    class Class1
+    {
+        public class Thing {
+            public void Stuff(int a, int b)
+            {
+                int ar = a / 2;
+                Console.WriteLine(b);
+
+              
+
+            }
+        }
+    }
+}
---
+++ 113Method2/113Method2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _113Method2.Class1;
+
+namespace _113Method2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thing stuff = new Thing();
+            int a = 5;
+            int b = 7;
+            stuff.Stuff(a: a, b: b);
+            Console.Read();
+
+        }
+    }
+}
