68671084cf69beebd66dd0559d1569eb4d79deb1
Talbot French <talbot.french@gmail.com>
2019-12-17 16:31:13 -0700

    117try2

---
+++ 117try2/117try2/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _117try2
+{
+    class Class1
+    {
+        public void Divide(int numA)
+        {
+            numA = numA / 2;
+
+        }
+
+        public void Divide(int numA, int numB)
+        {
+            int result = (numA + numB) / 2;
+
+        }
+
+
+        public static int numG;
+        static void Plain()
+        {
+            numG = 10;
+            Console.WriteLine(numG);
+        }
+    }
+}
+
---
+++ 117try2/117try2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _117try2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                DivideByTwo div = new DivideByTwo();
+
+                Console.WriteLine("Enter a number");
+                int A = Convert.ToInt32(Console.ReadLine());
+                div.Divide(numA: A);
+
+
+                Console.WriteLine("Enter another number");
+                int B = Convert.ToInt32(Console.ReadLine());
+                div.Divide(numA: A, numB: B);
+
+
+
+
+
+                Console.ReadLine();
+
+            }
+        }
+}
